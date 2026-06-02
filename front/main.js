52b735485aee07389d96f45d1a883d36aa264afb
Rafaël <rafael.vergnes@hotmail.com>
2026-06-02 19:20:27 +0200

    fix : bug filter feat

--- front/main.js
+++ front/main.js
@@ -36,77 +36,92 @@ console.log(theBoutonLogement);
 
 let isClicked_logement = false;
 
-let docsFinance = document.body.querySelector(".Finance");
-console.log(docsFinance);
+let docsFinance = document.body.getElementsByClassName("Finance");
+let tabDocsFinance = Array.from(docsFinance);
+console.log(tabDocsFinance);
 
-let docsEducation = document.body.querySelector(".Education");
-console.log(docsEducation);
+let docsEducation = document.body.getElementsByClassName("Education");
+let tabDocsEducation = Array.from(docsEducation);
+console.log(tabDocsEducation);
 
-let docsProfessionnel = document.body.querySelector(".Professionnel");
-console.log(docsProfessionnel);
+let docsProfessionnel = document.body.getElementsByClassName("Professionnel");
+let tabDocsProfessionnel = Array.from(docsProfessionnel);
+console.log(tabDocsProfessionnel);
 
-let docsPersonnel = document.body.querySelector(".Personnel");
-console.log(docsPersonnel);
+let docsPersonnel = document.body.getElementsByClassName("Personnel");
+let tabDocsPersonnel = Array.from(docsPersonnel);
+console.log(tabDocsPersonnel);
 
-let docsLogement = document.body.querySelector(".Logement");
-console.log(docsLogement);
+let docsLogement = document.body.getElementsByClassName("Logement");
+let tabDocsLogement = Array.from(docsLogement);
+console.log(tabDocsLogement);
 
 theBoutonFinance.addEventListener("click",(clickEvent)=>{
-    docsFinance.style.display = "flex";
     theBoutonFinance.style.backgroundColor = isClicked_finance ? "white" : "#004BD7"
     theBoutonFinance.style.color = isClicked_finance ? "black" : "white"
     theBoutonFinance.style.border = isClicked_finance ? "1px solid black" : "2px solid white"
-    docsEducation.style.display = isClicked_finance ? "flex" : "none"
-    docsProfessionnel.style.display = isClicked_finance ? "flex" : "none"
-    docsPersonnel.style.display = isClicked_finance ? "flex" : "none"
-    docsLogement.style.display = isClicked_finance ? "flex" : "none"
+
+    tabDocsFinance.map((document) => document.style.display = "flex");
+    tabDocsEducation.map((document) => document.style.display = isClicked_finance ? "flex" : "none");
+    tabDocsProfessionnel.map((document) => document.style.display = isClicked_finance ? "flex" : "none");
+    tabDocsPersonnel.map((document) => document.style.display = isClicked_finance ? "flex" : "none");
+    tabDocsLogement.map((document) => document.style.display = isClicked_finance ? "flex" : "none");
+
     isClicked_finance = !isClicked_finance;
 })
 
 theBoutonEducation.addEventListener("click",(clickEvent)=>{
-    docsEducation.style.display = "flex";
     theBoutonEducation.style.backgroundColor = isClicked_education ? "white" : "#004BD7"
     theBoutonEducation.style.color = isClicked_education ? "black" : "white"
     theBoutonEducation.style.border = isClicked_education ? "1px solid black" : "2px solid white"
-    docsFinance.style.display = isClicked_education ? "flex" : "none"
-    docsProfessionnel.style.display = isClicked_education ? "flex" : "none"
-    docsPersonnel.style.display = isClicked_education ? "flex" : "none"
-    docsLogement.style.display = isClicked_education ? "flex" : "none"
+
+    tabDocsEducation.map((document) => document.style.display = "flex");
+    tabDocsFinance.map((document) => document.style.display = isClicked_education ? "flex" : "none");
+    tabDocsProfessionnel.map((document) => document.style.display = isClicked_education ? "flex" : "none");
+    tabDocsPersonnel.map((document) => document.style.display = isClicked_education ? "flex" : "none");
+    tabDocsLogement.map((document) => document.style.display = isClicked_education ? "flex" : "none");
+
     isClicked_education = !isClicked_education;
 })
 
 theBoutonProfessionnel.addEventListener("click",(clickEvent)=>{
-    docsProfessionnel.style.display = "flex";
     theBoutonProfessionnel.style.backgroundColor = isClicked_professionnel ? "white" : "#004BD7"
     theBoutonProfessionnel.style.color = isClicked_professionnel ? "black" : "white"
     theBoutonProfessionnel.style.border = isClicked_professionnel ? "1px solid black" : "2px solid white"
-    docsFinance.style.display = isClicked_professionnel ? "flex" : "none"
-    docsEducation.style.display = isClicked_professionnel ? "flex" : "none"
-    docsPersonnel.style.display = isClicked_professionnel ? "flex" : "none"
-    docsLogement.style.display = isClicked_professionnel ? "flex" : "none"
+
+    tabDocsProfessionnel.map((document) => document.style.display = "flex");
+    tabDocsFinance.map((document) => document.style.display = isClicked_professionnel ? "flex" : "none");
+    tabDocsEducation.map((document) => document.style.display = isClicked_professionnel ? "flex" : "none");
+    tabDocsPersonnel.map((document) => document.style.display = isClicked_professionnel ? "flex" : "none");
+    tabDocsLogement.map((document) => document.style.display = isClicked_professionnel ? "flex" : "none");
+
     isClicked_professionnel = !isClicked_professionnel;
 })
 
 theBoutonPersonnel.addEventListener("click",(clickEvent)=>{
-    docsPersonnel.style.display = "flex";
     theBoutonPersonnel.style.backgroundColor = isClicked_personnel ? "white" : "#004BD7"
     theBoutonPersonnel.style.color = isClicked_personnel ? "black" : "white"
     theBoutonPersonnel.style.border = isClicked_personnel ? "1px solid black" : "2px solid white"
-    docsFinance.style.display = isClicked_personnel ? "flex" : "none"
-    docsEducation.style.display = isClicked_personnel ? "flex" : "none"
-    docsProfessionnel.style.display = isClicked_personnel ? "flex" : "none"
-    docsLogement.style.display = isClicked_personnel ? "flex" : "none"
+
+    tabDocsPersonnel.map((document) => document.style.display = "flex");
+    tabDocsFinance.map((document) => document.style.display = isClicked_personnel ? "flex" : "none");
+    tabDocsEducation.map((document) => document.style.display = isClicked_personnel ? "flex" : "none");
+    tabDocsProfessionnel.map((document) => document.style.display = isClicked_personnel ? "flex" : "none");
+    tabDocsLogement.map((document) => document.style.display = isClicked_personnel ? "flex" : "none");
+
     isClicked_personnel = !isClicked_personnel;
 })
 
 theBoutonLogement.addEventListener("click",(clickEvent)=>{
-    docsLogement.style.display = "flex";
     theBoutonLogement.style.backgroundColor = isClicked_logement ? "white" : "#004BD7"
     theBoutonLogement.style.color = isClicked_logement ? "black" : "white"
     theBoutonLogement.style.border = isClicked_logement ? "1px solid black" : "2px solid white"
-    docsFinance.style.display = isClicked_logement ? "flex" : "none"
-    docsEducation.style.display = isClicked_logement ? "flex" : "none"
-    docsProfessionnel.style.display = isClicked_logement ? "flex" : "none"
-    docsPersonnel.style.display = isClicked_logement ? "flex" : "none"
+
+    tabDocsLogement.map((document) => document.style.display = "flex");
+    tabDocsFinance.map((document) => document.style.display = isClicked_logement ? "flex" : "none");
+    tabDocsEducation.map((document) => document.style.display = isClicked_logement ? "flex" : "none");
+    tabDocsProfessionnel.map((document) => document.style.display = isClicked_logement ? "flex" : "none");
+    tabDocsPersonnel.map((document) => document.style.display = isClicked_logement ? "flex" : "none");
+
     isClicked_logement = !isClicked_logement;
 })
